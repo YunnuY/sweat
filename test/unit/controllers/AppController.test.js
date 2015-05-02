79b1182fa896166d9f43e4374d2edbbc57907096
feitian124 <feitian124@gmail.com>
2015-05-02 19:56:26 +0800

    add test for post

--- test/unit/controllers/AppController.test.js
+++ test/unit/controllers/AppController.test.js
@@ -1,26 +1,44 @@
 var request = require('supertest');
+var template = require('../../support').template;
+var tail = require('../../support').tail();
+var params = require('../../support').params();
 
 describe('AppController', function() {
 
-  describe('#auth()', function() {
-    it('should response echostr', function (done) {
-      var params = {
-        timestamp: new Date().getTime(),
-        nonce: parseInt((Math.random() * 10e10), 10)
-      };
-      var s = [sails.config.wechat.token, params.timestamp, params.nonce].sort().join('');
-      params.signature = require('crypto').createHash('sha1').update(s).digest('hex');
-      params.echostr = 'hehe';
+  describe('#wechat()', function() {
+    it('valid get should response echostr', function (done) {
       request(sails.hooks.http.app)
         .get('/')
         .query(params)
         .expect(200, params.echostr, done);
     });
-    it('should 401', function (done) {
+    it('invalid get should response 401', function (done) {
       request(sails.hooks.http.app)
       .get('/')
       .expect(401)
       .expect('Invalid signature', done);
+    });
+    it('valid post should ok', function (done) {
+      var info = {
+        sp: 'nvshen',
+        user: 'diaosi',
+        type: 'text',
+        text: '测试中'
+      };
+      request(sails.hooks.http.app)
+      .post('/' + tail)
+      .send(template(info))
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        var body = res.text.toString();
+        body.should.include('<ToUserName><![CDATA[diaosi]]></ToUserName>');
+        body.should.include('<FromUserName><![CDATA[nvshen]]></FromUserName>');
+        body.should.match(/<CreateTime>\d{13}<\/CreateTime>/);
+        body.should.include('<MsgType><![CDATA[text]]></MsgType>');
+        body.should.include('<Content><![CDATA[hehe]]></Content>');
+        done();
+      });
     });
   });
 
